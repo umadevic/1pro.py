5abe68fdf63eef95aa0291930a39bdde3bab9de8
umadevic <51950421+umadevic@users.noreply.github.com>
2019-07-24 23:39:10 +0530

    Update 30pro.py

--- 30pro.py
+++ 30pro.py
@@ -1,13 +1,13 @@
 #a
 
 n9=input()
-ca1=0
+cb=0
 for i in range(0,len(n9)):
-    ss1=n1[0:i]+n1[i+1::]
-    if int(ss1)%8==0:
-        ca1=1
+    ss=n9[0:i]+n9[i+1::]
+    if int(ss)%8==0:
+        cb=1
         break
-if ca1==1:
+if cb==1:
     print("yes")
 else:
     print("no")
